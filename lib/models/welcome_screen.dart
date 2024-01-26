79429aa45978dbeb1556e12bba2ec6845d72dd43
tomohiro1024 <gongyezhihong732@gmail.com>
2024-01-26 10:50:49 +0900

    first comimt

---
+++ lib/models/welcome_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: const [
+            Text("サッカークイズ"),
+          ],
+        ),
+      ),
+    );
+  }
+}
