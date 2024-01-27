6455c39639fa21ca7011c7e62b9a512589a0362e
tomohiro1024 <gongyezhihong732@gmail.com>
2024-01-27 12:07:09 +0900

    ステータスバーの作成

--- lib/screens/quiz/components/body.dart
+++ lib/screens/quiz/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -8,10 +9,35 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text("test"),
-        ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              Container(
+                width: double.infinity,
+                height: 35,
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Stack(
+                  children: [
+                    LayoutBuilder(
+                        builder: (context, constraints) => Container(
+                              width: constraints.maxWidth * 0.5,
+                              decoration: BoxDecoration(
+                                color: Colors.red,
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                            ))
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
