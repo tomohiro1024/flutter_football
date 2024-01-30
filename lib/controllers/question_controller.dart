2d1688d2262a913dcf0f5f5e7ba09d8ebd1d207d
tomohiro1024 <gongyezhihong732@gmail.com>
2024-01-30 18:48:04 +0900

    add checkAnswer()

--- lib/controllers/question_controller.dart
+++ lib/controllers/question_controller.dart
@@ -10,14 +10,31 @@ class QuestionController extends GetxController
 
   Animation get animation => _animation;
 
-  final List<Question> _questions = questionData.map((question) => Question(
-    id: question["id"],
-    question: question["question"],
-    options: question["options"],
-    answer: question["answer_index"],
-  )).toList();
+  final List<Question> _questions = questionData
+      .map((question) => Question(
+            id: question["id"],
+            question: question["question"],
+            options: question["options"],
+            answer: question["answer_index"],
+          ))
+      .toList();
 
   List<Question> get questions => _questions;
+
+  bool _isAnswered = false;
+  bool get isAnswered => _isAnswered;
+
+  int? _correctAnswer;
+  int? get correctAnswer => _correctAnswer;
+
+  int? _selectedAnswer;
+  int? get selectedAnswer => _selectedAnswer;
+
+  RxInt _questionNumber = 1.obs;
+  RxInt get questionNumber => _questionNumber;
+
+  int _numOfCorrectAnswer = 0;
+  int get numOfCorrectAns => _numOfCorrectAnswer;
 
   @override
   void onInit() {
@@ -33,5 +50,21 @@ class QuestionController extends GetxController
     _animationController.forward();
 
     super.onInit();
+  }
+
+  void checkAnswer(Question question, int selectedIndex) {
+    _isAnswered = true;
+    // 答えの番号
+    _correctAnswer = question.answer;
+    // ユーザーが選択した番号
+    _selectedAnswer = selectedIndex;
+
+    if (_correctAnswer == _selectedAnswer) {
+      // 正解した回数
+      _numOfCorrectAnswer++;
+    }
+
+    _animationController.stop();
+    update();
   }
 }
