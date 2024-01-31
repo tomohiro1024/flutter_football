a559326077bbc8e18314ba2271c95d6f60dfab31
tomohiro1024 <gongyezhihong732@gmail.com>
2024-01-31 10:43:19 +0900

    add getColor()

--- lib/screens/quiz/components/option.dart
+++ lib/screens/quiz/components/option.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_footbool/controllers/question_controller.dart';
+import 'package:get/get.dart';
 
 class Option extends StatelessWidget {
   const Option({
@@ -13,30 +15,53 @@ class Option extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: press,
-      child: Container(
-        margin: const EdgeInsets.only(top: 25),
-        padding: const EdgeInsets.all(25),
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.black),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text("${index + 1} $text"),
-            Container(
-              height: 20,
-              width: 20,
+    return GetBuilder<QuestionController>(
+        init: QuestionController(),
+        builder: (qnController) {
+          Color getColor() {
+            // isAnsweredがtrueの場合(選択肢をタップした場合)
+            if (qnController.isAnswered) {
+              // 正解をタップした場合、緑色を取得
+              if (index == qnController.correctAnswer) {
+                return Colors.green;
+              } else if // 不正解をタップした場合、赤色を取得
+                  (index == qnController.selectedAnswer &&
+                      qnController.selectedAnswer !=
+                          qnController.correctAnswer) {
+                return Colors.red;
+              }
+            }
+            return Colors.black;
+          }
+
+          return InkWell(
+            onTap: press,
+            child: Container(
+              margin: const EdgeInsets.only(top: 25),
+              padding: const EdgeInsets.all(25),
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(40),
-                border: Border.all(color: Colors.green),
+                border: Border.all(color: getColor()),
+                borderRadius: BorderRadius.circular(10),
               ),
-            )
-          ],
-        ),
-      ),
-    );
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "${index + 1} $text",
+                    style: TextStyle(color: getColor()),
+                  ),
+                  Container(
+                    height: 20,
+                    width: 20,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      border: Border.all(color: getColor()),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
