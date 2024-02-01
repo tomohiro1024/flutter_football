a7ed56247368e996ca25389852fbc8c3ee3c0d4d
tomohiro1024 <gongyezhihong732@gmail.com>
2024-02-01 19:05:32 +0900

    add icons

--- lib/screens/quiz/components/option.dart
+++ lib/screens/quiz/components/option.dart
@@ -34,33 +34,47 @@ class Option extends StatelessWidget {
             return Colors.black;
           }
 
+          IconData getIcon() {
+            return getColor() == Colors.red
+                ? Icons.clear
+                : Icons.radio_button_unchecked;
+          }
+
           return InkWell(
             onTap: press,
-            child: Container(
-              height: 70,
-              margin: const EdgeInsets.only(top: 20),
-              padding: const EdgeInsets.all(25),
-              decoration: BoxDecoration(
-                border: Border.all(color: getColor()),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "${index + 1} $text",
-                    style: TextStyle(color: getColor()),
+            child: Row(
+              children: [
+                Container(
+                  height: 70,
+                  width: 290,
+                  margin: const EdgeInsets.only(top: 20),
+                  padding: const EdgeInsets.all(25),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: getColor()),
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  Container(
-                    height: 20,
-                    width: 20,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(40),
-                      border: Border.all(color: getColor()),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "${index + 1} $text",
+                        style: TextStyle(color: getColor()),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                    margin: const EdgeInsets.only(top: 20, left: 10),
+                    child: getColor() == Colors.black
+                        ? null
+                        : Icon(
+                            getIcon(),
+                            color: getColor() == Colors.black
+                                ? Colors.transparent
+                                : getColor(),
+                          )
                     ),
-                  )
-                ],
-              ),
+              ],
             ),
           );
         });
