5a302a8554ca09702a003c529bd5e6a8f0374e52
tomohiro1024 <gongyezhihong732@gmail.com>
2024-02-01 13:09:49 +0900

    回答選択肢を一つ減らす

--- lib/models/Questions.dart
+++ lib/models/Questions.dart
@@ -13,25 +13,25 @@ const List questionData = [
     "id": 1,
     "question":
         teamName,
-    "options": ['Apple', 'Google', 'Facebook', 'Microsoft'],
+    "options": ['Apple', 'Google', 'Facebook'],
     "answer_index": 1,
   },
   {
     "id": 2,
     "question": teamName,
-    "options": ['Jun 2017', 'Jun 2017', 'May 2017', 'May 2018'],
+    "options": ['Jun 2017', 'Jun 2017', 'May 2017'],
     "answer_index": 2,
   },
   {
     "id": 3,
     "question": teamName,
-    "options": ['Double', 'Int', 'Char', 'Word'],
+    "options": ['Double', 'Int', 'Char'],
     "answer_index": 2,
   },
   {
     "id": 4,
     "question": teamName,
-    "options": ['Cin', 'Count>>', 'Cout', 'Output>>'],
+    "options": ['Cin', 'Count>>', 'Cout'],
     "answer_index": 2,
   },
 ];
